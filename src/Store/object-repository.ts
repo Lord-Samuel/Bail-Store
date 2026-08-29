fc235db637d1802dc268736154d83237a78a038e
Samuel -!Rebix <lordsamueltech@gmail.com>
2026-08-29 18:28:39 +0000

    Refactor ObjectRepository class to remove generics

--- src/Store/object-repository.ts
+++ src/Store/object-repository.ts
@@ -1,32 +1,31 @@
-export class ObjectRepository<T extends object> {
-	readonly entityMap: Map<string, T>
+class ObjectRepository {
+    constructor(entities = {}) {
+        this.entityMap = new Map(Object.entries(entities));
+    }
 
-	constructor(entities: Record<string, T> = {}) {
-		this.entityMap = new Map(Object.entries(entities))
-	}
+    findById(id) {
+        return this.entityMap.get(id);
+    }
 
-	findById(id: string) {
-		return this.entityMap.get(id)
-	}
+    findAll() {
+        return Array.from(this.entityMap.values());
+    }
 
-	findAll() {
-		return Array.from(this.entityMap.values())
-	}
+    upsertById(id, entity) {
+        return this.entityMap.set(id, { ...entity });
+    }
 
-	upsertById(id: string, entity: T) {
-		return this.entityMap.set(id, { ...entity })
-	}
+    deleteById(id) {
+        return this.entityMap.delete(id);
+    }
 
-	deleteById(id: string) {
-		return this.entityMap.delete(id)
-	}
+    count() {
+        return this.entityMap.size;
+    }
 
-	count() {
-		return this.entityMap.size
-	}
-
-	toJSON() {
-		return this.findAll()
-	}
-
+    toJSON() {
+        return this.findAll();
+    }
 }
+
+module.exports = { ObjectRepository };
